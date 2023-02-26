442988ded8d33256ce8d2a49844fea3c9fc3b404
TringApps-Nithish <123442599+TringApps-Nithish@users.noreply.github.com>
2023-02-27 00:00:02 +0530

    Update A1_with_EX.java

--- A1_with_EX.java
+++ A1_with_EX.java
@@ -10,9 +10,10 @@ class details {
     int accnum;
     int bal, dpamount, wdamount;
 
-    details(String name, int an) {
+    details(String name, int an,int balance) {
         holdername = name;
         accnum = an;
+	bal = balance;
     }
 
     void deposite() {
@@ -25,7 +26,7 @@ class details {
         try {
             System.out.printf("\nEnter Amount You Want To WithDraw : ");
             wdamount = sc.nextInt();
-            if (wdamount > dpamount)
+            if (wdamount > bal)
                 throw new UnsufficientAmountException();
             else
                 bal -= wdamount;
@@ -40,19 +41,21 @@ class details {
 
 }
 
-class BankAccount {
+class A1_with_Ex {
     public static void main(String[] args) {
 
         String holdername;
-        int accnum, balance, ch = 0;
+        int accnum,balance,ch = 0;
         Scanner sc = new Scanner(System.in);
         System.out.print("\t\t\t ----- Welcome To YOUR-BANK -----\n");
         System.out.printf("Enter the Account Holder Name :  ");
         holdername = sc.nextLine();
         System.out.printf("Enter the Account Number :  ");
         accnum = sc.nextInt();
+	System.out.printf("Enter Your Account Balance :  ");
+        balance = sc.nextInt();
 
-        details ob = new details(holdername, accnum);
+        details ob = new details(holdername, accnum,balance);
 
         while (ch != 4) {
             System.out.printf(
@@ -81,5 +84,6 @@ class BankAccount {
                     break;
             }
         }
+        sc.close();
     }
 }
